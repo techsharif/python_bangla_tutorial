5c54e42b2e9d1889ab54b8befdcf2f3069f52c1d
techsharif <sharif.cse.hstu@gmail.com>
2017-09-05 10:17:30 +0600

    add code from 1.3 to 2.04

---
+++ 2.01/comment.py
@@ -0,0 +1,10 @@
+# type in terminal - python3 comment.py
+#print('Hello World') # this is single line comment
+
+'''
+go to python practice folder
+type python3 commnet.py
+
+'''
+
+print('Hello World')
