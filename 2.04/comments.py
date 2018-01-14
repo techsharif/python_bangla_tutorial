1d7a42baa4d52f6d3b280d8c80b8372b9e0eb940
techsharif <sharif.cse.hstu@gmail.com>
2018-01-14 23:44:17 +0600

    upadate upto  2.07

---
+++ 2.04/comments.py
@@ -0,0 +1,8 @@
+"""
+Author: Sharif Chowdhury
+Language: Python 3
+
+"""
+
+print("hello") # this is double quote print function
+print('hello') # this is single quote print function
--- 2.04/rules_and_guidelines.py
+++
@@ -1,2 +0,0 @@
-print(5)
-Print(5)
