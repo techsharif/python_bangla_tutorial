67686edcd06c9e2221b83ab3cc0150f216114c11
Md. Sharif Chowdhury <sharif.cse.hstu@gmail.com>
2018-10-01 08:12:41 +0600

    add 2.12 print once again files

---
+++ 2.12/print_02_end.py
@@ -0,0 +1,37 @@
+# Number Print
+# print(4)
+# print(1.2)
+# a = 3.4
+# print(a)
+
+# String Print
+# print("this is a string")
+
+# Multiple value print
+# a = 2.3
+# b = 5
+# c = "hello"
+
+# print(a,b,c)
+
+
+# print -> it's ok to print 'single' quote
+# print("it's ok to print 'single' quote")
+
+
+# print -> No problem to print "double" quote
+# print('No problem to print "double" quote')
+
+# print -> it's ok to print both 'single' and "double" quote
+# print('it\'s ok to print both \'single\' and "double" quote')
+# print(''' it's ok to print both 'single' and "double" quote ''')
+# print(""" it's ok to print both 'single' and "double" quote """)
+
+
+# print more than one line
+print("""
+
+this is line 1
+this is line 2
+
+	""")
---
+++ 2.12/print_02_start.py
@@ -0,0 +1,23 @@
+# Number Print
+
+
+# String Print
+
+
+# Multiple value print
+
+
+
+# print -> it's ok to print 'single' quote
+
+
+
+# print -> No problem to print "double" quote
+
+
+# print -> it's ok to print both 'single' and "double" quote
+
+
+
+# print more than one line
+
