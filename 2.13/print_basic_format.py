223d7c7d10bc26a225d210c0ab9bc0974fb67da3
Sharif Chowdhury <sharif.bkash2019@gmail.com>
2019-05-19 00:51:34 +0600

    add 2.13 and gitignore file

---
+++ 2.13/print_basic_format.py
@@ -0,0 +1,12 @@
+name = "Hasan"
+student_id = 1202001
+mark_average = 74.3423
+
+
+# Hasan 1202001 74.3423
+#print(name, student_id, mark_average)
+
+
+
+# Hasan 1202001 74.34
+print("{:s} {:d} {:.2f}".format(name, student_id, mark_average))
